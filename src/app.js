1fdfb7172bf254096d7a9b523387e533f69a072c
Your Name <you@example.com>
2017-12-27 19:33:06 +0530

    add ws server

--- src/app.js
+++ src/app.js
@@ -13,3 +13,22 @@ let textView = new TextView({
 button.on('select', () => {
   textView.text = 'Tabris.js rocks!';
 });
+
+webserver.onRequest(
+	function(request) {
+		console.log("O MA GAWD! This is the request: ", request);
+
+		webserver.sendResponse(
+			request.requestId,
+			{
+				status: 200,
+				body: '<html>Hello World</html>',
+				headers: {
+					'Content-Type': 'text/html'
+				}
+			}
+		);
+	}
+);
+
+webserver.start();
